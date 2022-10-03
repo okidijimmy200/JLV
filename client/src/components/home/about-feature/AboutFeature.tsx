894e142b99ddd365157c2f30a4e13f0f37ef467d
okidijimmy200 <okidijimmie@gmail.com>
2022-10-03 12:46:09 +0300

    final-3

--- client/src/components/home/about-feature/AboutFeature.tsx
+++ client/src/components/home/about-feature/AboutFeature.tsx
@@ -53,7 +53,7 @@ export default function AboutFeature({farm, diary, resort, onclick, diaryHandler
                         Nestled in the gently sloping hills of Rubaya, Kashari, Mbarara district, lies JLV Mixed farm: A
                         model Farm like no other! Featuring a wide array of livestock, food crops, cash crops and all
                         things natural, we are a haven for all avid farmers. Be it someone who is willing to learn about
-                        farming from scratch, or someone trying to improve on the knowledge they already have.
+                        farming and from scratch, or someone trying to improve on the knowledge they already have.
                         </p>
                         </div>
 }
